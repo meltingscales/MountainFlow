b884f039bbb8833b4b116060bfcb45228c710d90
henryfbp <HenryFBP@gmail.com>
2022-03-12 13:50:31 -0500

    get sprites to work

--- source/Player.hx
+++ source/Player.hx
@@ -12,7 +12,14 @@ class Player extends FlxSprite
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
-		makeGraphic(16, 16, FlxColor.BLUE);
+
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(LEFT, false, false);
+		setFacingFlip(RIGHT, true, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
+
 		drag.x = drag.y = 1600;
 	}
 
@@ -50,6 +57,30 @@ class Player extends FlxSprite
 			var angle = IHateMath.angleOfTheDangle(up, down, left, right);
 			velocity.set(SPEED, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), angle);
+
+			if (up)
+				facing = UP;
+			else if (down)
+				facing = DOWN;
+			else if (left)
+				facing = LEFT;
+			else if (right)
+				facing = RIGHT;
+
+			if ((velocity.x != 0 || velocity.y != 0) && touching == NONE)
+			{
+				switch (facing)
+				{
+					case LEFT, RIGHT:
+						animation.play("lr");
+					case UP:
+						animation.play("u");
+					case DOWN:
+						animation.play("d");
+					case _:
+						"";
+				}
+			}
 		}
 	}
 }
