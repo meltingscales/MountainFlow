4e24cff0e1813eda690bf65d70cb0513b1b0d465
henryfbp <HenryFBP@gmail.com>
2022-03-12 11:01:48 -0500

    format

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,8 +11,6 @@ class PlayState extends FlxState
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
-
-		
 	}
 
 	override public function update(elapsed:Float)
