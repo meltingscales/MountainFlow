f27bf5a5742274244fd803e235c9df3b148fccba
Henry Post <HenryFBP@gmail.com>
2022-03-26 19:37:28 -0400

    fix stdout redir

--- .github/scripts/install-haxeflixel.sh
+++ .github/scripts/install-haxeflixel.sh
@@ -5,16 +5,16 @@ sudo apt-get install haxe -y
 haxelib setup ~/haxelib
 echo "Installing lime, openfl, and flixel..."
 
-haxelib install lime 2>&1 > limelog.txt
+haxelib install lime > limelog.txt 2>&1
 cat limelog.txt
 
-haxelib install openfl 2>&1 > openfllog.txt
+haxelib install openfl > openfllog.txt 2>&1 
 cat openfllog.txt
 
-haxelib install flixel 2>&1 > flixellog.txt
+haxelib install flixel > flixellog.txt 2>&1
 cat flixellog.txt
 
-haxelib run lime setup flixel 2>&1 > limesetupflixellog.txt
+haxelib run lime setup flixel > limesetupflixellog.txt 2>&1 
 cat limesetupflixellog.txt
 
 echo "Done downloading."
